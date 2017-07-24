fd57e511835b8a386c544e3961dc4296c6b2b17a
Fred Campos <mail2frede@gmail.com>
2017-07-24 16:27:50 -0300

    [WIP]: Core rest client including new methods

--- hubstry/core.py
+++ hubstry/core.py
@@ -1,11 +1,40 @@
-
-from .settings import Conf
 import json
+import re
 import requests
+from .settings import Conf
 
 
 class RegistryNotFound(BaseException):
     pass
+
+
+class RegistryUnexpectedResponse(BaseException):
+    pass
+
+
+class Request(object):
+
+    @staticmethod
+    def validate_response_status(response):
+        if response.status_code != 200:
+            error_msg = "%Returned status code %d, Body %s" % \
+                (response.status_code, json.dumps(response.json()))
+            raise RegistryUnexpectedResponse(error_msg)
+
+    @staticmethod
+    def get(url, index, error_msg):
+        response = requests.get(url)
+        Request.validate_response_status(response)
+        content = response.json()
+        if content.get(index):
+            raise RegistryNotFound(error_msg)
+
+        return content.get(index)
+
+    @staticmethod
+    def delete(url):
+        response = requests.delete(url)
+        Request.validate_response_status(response)
 
 
 class Registry(object):
@@ -13,23 +42,51 @@ class Registry(object):
     def __init__(self):
         self.api_url = "%s/%s" % (Conf.Registry.URL, Conf.Registry.API_VERSION)
 
+    def healthcheck(self):
+        url = '%s/' % self.api_url
+        response = requests.get(url)
+        Request.validate_response_status(response)
+        return {'status': 200, 'message': 'Ok'}
+
     def images(self, limit=20, last_image=None):
         if not last_image:
             last = ''
         else:
             last = '&last=' % last_image
         url = '%s/_catalog?n=%s%s' % (self.api_url, limit, last)
+        content = Request.get(url=url,
+                              index='repositories',
+                              error_msg='No image(s) found at catalog.')
+        return content
 
-        response = requests.get(url)
-        content = json.loads(response.content.decode())
+    def find_image(self, name_to_find):
+        images = self.images()
+        r = re.compile(name_to_find)
+        result = filter(r.match, images)
+        return result
 
-        if not content or not content.get('repositories'):
-            raise RegistryNotFound("No image(s) have been pushed!")
+    def get_image_tags(self, name):
+        url = '%s/%s/tags/list' % (self.api_url, name)
+        error_msg = 'No tag(s) found at image %s' % name
+        content = Request.get(url=url,
+                              index='tags',
+                              error_msg=error_msg)
 
-        return content.get('repositories')
+        return content
 
-    def get_image_by_name(self, name):
+    def get_tag_manifests(self, name, tag):
+        url = '{0}/{1}/manifests/{2}'.format(self.api_url, name, tag)
 
-        url = '%s/%s' % (self.api_url, name)
+        error_msg = \
+            'No manifests found for image {0} and tag {1}'.format(name, tag)
+        content = Request.get(url=url,
+                              index='fsLayers',
+                              error_msg=error_msg)
 
-        response = requests.get(url)
+        return content
+
+    def delete_tag(self, name, tag):
+        url = '{0}/{1}/manifests/{2}'.format(self.api_url, name, tag)
+
+        self.get_tag_manifests(name, tag)
+        Request.delete(url=url)
--- hubstry/tests/unit/test_core.py
+++ hubstry/tests/unit/test_core.py
@@ -1,13 +1,31 @@
 import unittest
-from hubstry.core import Registry
+import pytest
+from hubstry.core import (
+    Registry,
+    RegistryNotFound,
+    RegistryUnexpectedResponse
+)
 
 
 class TestRegistry(unittest.TestCase):
 
+    def test_healthcheck_should_return_ok(self):
+        registry = Registry()
+        result = registry.healthcheck()
+        self.assertEqual(result.get('status'), 200)
+        self.assertEqual(result.get('message'), 'Ok')
+
+    @pytest.mark.skip('not implemented yet')
+    def test_healthcheck_should_raise_exception(self):
+        registry = Registry()
+        self.assertRaises(RegistryUnexpectedResponse, registry.healthcheck)
+
+    @pytest.mark.skip('not implemented yet')
     def test_images_should_make_request(self):
         registry = Registry()
         registry.images()
 
+    @pytest.mark.skip('not implemente yet')
     def test_get_image_by_name_should_retrieve_result(self):
         registry = Registry()
         registry.get_image_by_name('python')
